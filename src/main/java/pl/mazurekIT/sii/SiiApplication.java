84c5b7544d66134a05da372247ad67591c79847f
Mazzi <mazzi198913@wp.pl>
2019-05-17 23:20:54 +0100

    get started

---
+++ src/main/java/pl/mazurekIT/sii/SiiApplication.java
@@ -0,0 +1,14 @@
+package pl.mazurekIT.sii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SiiApplication.class, args);
+    }
+
+}
